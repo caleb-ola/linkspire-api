94c7c70fd6c8de838327eb922c4753221b5ee581
caleb-ola <olajiire2@gmail.com>
2024-02-25 14:34:50 +0000

    feat: Added update user links feature

--- controllers/linkController.ts
+++ controllers/linkController.ts
@@ -3,6 +3,7 @@ import BadRequestError from "../Errors/badRequestError";
 import Link from "../models/linkModel";
 import AsyncHandler from "../utils/asyncHandler";
 import User from "../models/userModel";
+import NotAuthorizedError from "../Errors/notAuthorizedError";
 
 interface CustomRequest extends Request {
   currentUser?: any;
@@ -29,7 +30,6 @@ export const createLink: RequestHandler = AsyncHandler(
     await newLink.save();
 
     usr.links.push(newLink.id);
-
     await usr.save();
 
     res.status(200).json({
@@ -39,5 +39,53 @@ export const createLink: RequestHandler = AsyncHandler(
         data: newLink,
       },
     });
+  }
+);
+
+export const getUserLinks: RequestHandler = AsyncHandler(
+  async (req: CustomRequest, res, next) => {
+    const { currentUser } = req;
+    if (!currentUser) throw new BadRequestError("You are not logged in");
+
+    const userLinks = await Link.find({ user: currentUser.id });
+
+    res.status(200).json({
+      status: "success",
+      results: userLinks.length,
+      data: {
+        data: userLinks,
+      },
+    });
+  }
+);
+
+export const getSingleUserLink: RequestHandler = AsyncHandler(
+  async (req: CustomRequest, res, next) => {
+    const { id } = req.params;
+    const { currentUser } = req;
+    if (!currentUser) throw new BadRequestError("You are not logged in");
+
+    if (!currentUser.links.includes(id))
+      throw new NotAuthorizedError("You can only view your links");
+
+    const userLink = await Link.findById(id);
+    if (!userLink) throw new BadRequestError("Link not found");
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        data: userLink,
+      },
+    });
+  }
+);
+
+export const updateLink: RequestHandler = AsyncHandler(
+  async (req: CustomRequest, res, next) => {
+    const { title, url, description } = req.body;
+    const { id } = req.params;
+
+    const { currentUser } = req;
+    if (!currentUser) throw new BadRequestError("You are not logged in");
   }
 );
--- routers/linkRoutes.ts
+++ routers/linkRoutes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import protect from "../middlewares/protect";
-import { createLink } from "../controllers/linkController";
+import {
+  createLink,
+  getSingleUserLink,
+  getUserLinks,
+} from "../controllers/linkController";
 
 const router = Router();
 
-router.post("/", protect, createLink);
+router.route("/").post(protect, createLink).get(protect, getUserLinks);
+router.route("/:id").get(protect, getSingleUserLink);
 
 export default router;
