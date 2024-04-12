4d33e8b485b833bdde91b91990fd26fce9a26557
caleb-ola <olajiire2@gmail.com>
2024-04-12 15:17:38 +0100

    test: Implemented tests for link creation

--- tests/link.test.ts
+++ tests/link.test.ts
@@ -4,8 +4,10 @@ import Link from "../models/linkModel";
 import { userLogin } from "./setups/auth.setup";
 import supertest from "supertest";
 
+let data;
+
 const createLink = async () => {
-  const link = {
+  const data = {
     title: faker.word.adverb(),
     url: faker.internet.url(),
     user: faker.database.mongodbObjectId(),
@@ -15,41 +17,59 @@ const createLink = async () => {
   const response = await supertest(app)
     .post("/api/v1/links")
     .set("Authorization", `Bearer ${((await userLogin()) as any).token}`)
-    .send(link)
+    .send(data)
     // .field("title", link.title)
     // .field("url", link.url)
     // .field("user", ((await userLogin()) as any).data.data.id)
     // .field("description", link.description)
     .expect(201);
-
   return response;
 };
 
 describe("links", () => {
-  beforeEach(async () => {
-    await createLink();
-  }, 10000);
+  describe("Retrieve all links", () => {
+    beforeEach(async () => {
+      await createLink();
+    }, 10000);
 
-  describe("get link route", () => {
-    describe("given the link do not exist", () => {
-      it("should return a 404 error", async () => {
-        // expect(true).toBe(true);
-        const linkId = "link-123456";
+    it("should return a 200 status code", async () => {
+      await supertest(app)
+        .get(`/api/v1/links`)
+        .set("Authorization", `Bearer ${((await userLogin()) as any).token}`)
+        .expect(200);
+    });
+  });
 
-        await supertest(app).get(`/api/v1/links/${linkId}`).expect(404);
-      });
+  describe("Retrieve single link by id", () => {
+    beforeEach(async () => {
+      await createLink();
+    }, 10000);
+    it("should return a 404 error", async () => {
+      // expect(true).toBe(true);
+      const linkId = "link-123456";
+      await supertest(app).get(`/api/v1/links/${linkId}`).expect(404);
+    });
+  });
+
+  describe("Create link route", () => {
+    let link: any;
+    beforeEach(async () => {
+      link = await createLink();
     });
 
-    // describe("given the link does exist", async () => {
-    //   it("should return a 200 status code and the newly created link", async () => {
-    //     // console.log(linkObj);
-    //     const { status, body } = await supertest(app)
-    //       .get(`/api/v1/links/${linkId}`)
-    //       .set("Accept", "application/json")
-    //       .set("Authorization", `Bearer ${await userLogin()}`)
-    //       .expect(200);
-    //     expect(status).toBe(200);
-    //   });
-    // });
+    it("Should return 201 on successful creation", () => {
+      expect(link.status).toBe(201);
+    });
+
+    it("should have a status of success", () => {
+      expect(link._body.status).toBe("success");
+    });
+
+    it("should have certain fields", () => {
+      expect(link._body.data.data).toHaveProperty("title");
+      expect(link._body.data.data).toHaveProperty("user");
+      expect(link._body.data.data).toHaveProperty("url");
+      expect(link._body.data.data).toHaveProperty("description");
+    });
   });
 });
